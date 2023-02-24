2656d48018ed1866bd9a65d0ec874d13bc8087e2
Courtney McNair <camcnair@ad.unc.edu>
2023-02-23 20:23:40 -0500

    Progress on EX04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,29 +1,34 @@
-"""Exercise 04: Utils"""
+"""Exercise 04: Utils."""
 __author__ = "730408141"
 
+
 def all(x: list[int], y: int) -> bool:
-    """Indicates whether the int is the same as all ints in the list"""
+    """Indicates whether the int is the same as all ints in the list."""
     list_idx: int = 0
+    if len(x) == 0:
+        return False
     while list_idx < len(x):
         if y != x[list_idx]:
             return False
         list_idx += 1
     return True
 
+
 def max(input: list[int]) -> int:
-    """Returns the largest number in a list"""
+    """Returns the largest number in a list."""
     if len(input) == 0:
-        raise ValueError("max() arg is an empty List")
+        raise ValueError("max() arg is an empty List.")
     input_idx: int = 0
-    max: int = 0
+    max: int = input[input_idx]
     while input_idx < len(input):
-        if input[input_idx] > input[input_idx + 1]:
-            max = input[input_idx + 1]
+        if input[input_idx] > max:
+            max = input[input_idx]
         input_idx += 1
     return max
 
+
 def is_equal(list1: list[int], list2: list[int]) -> bool:
-    """Indicates whether 2 lists are equal to each other"""
+    """Indicates whether 2 lists are equal to each other."""
     idx: int = 0
     if len(list1) != len(list2):
         return False
