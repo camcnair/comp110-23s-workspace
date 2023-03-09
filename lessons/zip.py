c3616d79d7caa5a32166e2d0a8ed25feab8e0cbc
Courtney McNair <camcnair@ad.unc.edu>
2023-03-09 09:10:09 -0500

    CQ04

--- lessons/zip.py
+++ lessons/zip.py
@@ -1,4 +1,6 @@
 """CQ04."""
+__author__ = "730408141"
+
 
 def zip(keys: list[str], values: list[int]) -> dict[str,int]:
     """Produces a dictionary using the 2 lists."""
@@ -9,6 +11,3 @@ def zip(keys: list[str], values: list[int]) -> dict[str,int]:
         return dictionary
     for idx in range(0, len(keys)):
         dictionary[keys[idx]] = values[idx]
-
-
-zip(["one", "two", "three", "four"], [1, 2, 3, 4])
