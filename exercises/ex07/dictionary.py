e861a3f31e5239320f5c0a4e62804b084cc4e08f
Courtney McNair <camcnair@ad.unc.edu>
2023-03-23 16:59:52 -0400

    progress on ex07

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -7,7 +7,7 @@ def invert(input_dict: dict[str, str]) -> dict[str, str]:
     new_dict: dict[str, str] = {}
     for key in input_dict:
         value: str = input_dict[key]
-        if (value in new_dict) is True:
+        if value in new_dict:
             raise ValueError("There cannot be more than one of the same key.")
         new_dict[value] = key
     return new_dict
@@ -20,11 +20,11 @@ def favorite_color(input_dict: dict[str, str]) -> str:
     highest_count: int = 0
     for key in input_dict:
         color: str = input_dict[key]
-        if (color in counts) is True:  #adds 1 more for the count of that color
+        if color in counts:  # adds 1 more for the count of that color
             counts[color] += 1
-        else:  #color has not already been counted
+        else:  # color has not already been counted
             counts[color] = 1
-        if counts[color] > highest_count:  #establishes the color with the new highest count
+        if counts[color] > highest_count:  # establishes the color with the new highest count
             favorite_color = color
             highest_count = counts[color]    
     return favorite_color
@@ -34,8 +34,8 @@ def count(input_list: list[str]) -> dict[str, int]:
     """Counts the number of times a value appears in a given list."""
     counts: dict[str, int] = {}
     for item in input_list:
-        if item in counts:  #item already exists in counts
+        if item in counts:  # item already exists in counts
             counts[item] += 1
-        else:  #item doesn't exist in counts already, value is established 
+        else:  # item doesn't exist in counts already, value is established 
             counts[item] = 1
     return counts
