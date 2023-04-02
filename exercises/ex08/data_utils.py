442ad349ef59a0bbb495b2bf542493f358c182d5
Courtney McNair <camcnair@ad.unc.edu>
2023-04-02 14:01:38 -0400

    progress on ex08

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -1,9 +1,9 @@
 from csv import DictReader
 
 
-def read_csv_rows(filename: str) -> list[dict[str,str]]:
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
     """Read csv file and return as a list of dicts with header keys."""
-    result: list[dict[str,str]] = []
+    result: list[dict[str, str]] = []
     file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
@@ -12,23 +12,23 @@ def read_csv_rows(filename: str) -> list[dict[str,str]]:
     return result
 
 
-def column_values(table: list[dict[str,str]], header: str) -> list[str]:
+def column_values(table: list[dict[str, str]], header: str) -> list[str]:
     """Returns values in a table column under a specific header."""
     result: list[str] = []
-    #step through table
+    # step through table
     for row in table:
-         #save every value under key "header"
-         result.append(row[header])
+        # save every value under key "header"
+        result.append(row[header])
     return result
 
 
-def columnar(table: list[dict[str,str]]) -> dict[str,list[str]]:
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
     """Reformats data so that it's a dictionary with column headers as keys."""
-    result: dict[str,list[str]] = {}
-    #loop through keys of one row of table
-    first_row: dict[str,str] = table[0]
+    result: dict[str, list[str]] = {}
+    # loop through keys of one row of table
+    first_row: dict[str, str] = table[0]
     for key in first_row:
-        #for each key, make a dictionary entry with all column values
+        # for each key, make a dictionary entry with all column values
         result[key] = column_values(table, key)
     return result
 
